019fe9d2eb15cf8e3bfe1199a2e22961c234a16e
max-63 <133398922+max-63@users.noreply.github.com>
2025-06-21 09:12:15 +0200

    commit pour le css

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { showToast, ToastOptions } from "./toast.js";
+const path_relatif = "../dist/styles.css";
+const path_complet = new URL(path_relatif, import.meta.url).href;
 
 // ajouter le link du css dans le html
-document.head.innerHTML += `<link rel="stylesheet" href="../dist/styles.css" data="">`;
+document.head.innerHTML += `<link rel="stylesheet" href="" data="">`;
 
 export function notify(options: ToastOptions): void {
   showToast(options);
