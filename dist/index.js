8aba780c141d9aa78024093f28020a4987cd0603
max-63 <133398922+max-63@users.noreply.github.com>
2025-06-21 09:22:34 +0200

    commit

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@ import { showToast } from "./toast.js";
 const path_relatif = "../dist/styles.css";
 const path_complet = new URL(path_relatif, import.meta.url).href;
 // ajouter le link du css dans le html
-document.head.innerHTML += `<link rel="stylesheet" href="" data="">`;
+document.head.innerHTML += `<link rel="stylesheet" href="${path_complet}" data="">`;
 export function notify(options) {
     showToast(options);
 }
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const path_relatif = "../dist/styles.css";
 const path_complet = new URL(path_relatif, import.meta.url).href;
 
 // ajouter le link du css dans le html
-document.head.innerHTML += `<link rel="stylesheet" href="" data="">`;
+document.head.innerHTML += `<link rel="stylesheet" href="${path_complet}" data="">`;
 
 export function notify(options: ToastOptions): void {
   showToast(options);
